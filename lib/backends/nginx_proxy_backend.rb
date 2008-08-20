3e61a457c7c529686073da2fe14dd0ef636e1631
James Stradling <james@katipo.co.nz>
2008-08-20 15:41:49 +1200

    Bug fix: The correct file path is now sent in the X-Accel-Redirect header.

--- lib/backends/nginx_proxy_backend.rb
+++ lib/backends/nginx_proxy_backend.rb
@@ -4,10 +4,14 @@ module Backends
     def send_file_via_proxy(path, options)
       logger.info "Streaming file #{path} using Nginx's X-Accel-Redirect" unless logger.nil?
       
-      headers["Cache-Control"] = "private"
-      headers["Content-Disposition"] = %(#{options[:disposition]}; filename=#{options[:filename]};)
-      headers["Content-Type"] = options[:type].strip
-      headers["X-Accel-Redirect"] = "#{path}"
+      # Nginx doesn't want the full path to the file, just the path from the
+      # site root onwards..
+      path.gsub!(RAILS_ROOT, "")
+      
+      head "200 OK", 
+        "X-Accel-Redirect" => "#{path}", 
+        "Content-Disposition" => %(#{options[:disposition]}; filename=#{options[:filename]};),
+        "Content-Type" => options[:type].strip
       
       # Prevent .html extension from being appended to the filename we suggested above.
       @performed_render = true
